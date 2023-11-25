a6f20c57c0f8c70893e6f3249657b83ab6444b39
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-25 12:00:11 +0400

    methods fix :alien:

--- labExamQ3.java
+++ labExamQ3.java
@@ -12,19 +12,21 @@ class Employee {
     }
 
     double calculateTotalSalary() {
-        return this.salary + this.bonus;
+        return salary + bonus;
     }
 
     void display() {
-        System.out.println("The total salary is " + calculateTotalSalary());
+        System.out.println("Salary: " + salary);
+        System.out.println("Bonus: " + bonus);
+        System.out.println("Total Salary: " + calculateTotalSalary());
     }
 }
 
 class EmployeeMain {
     public static void main(String[] args) {
         Employee emp = new Employee();
-        emp.inputValues(3898.13, 545.3);
-        emp.calculateTotalSalary();
+        emp.inputValues(2000.0, 1600.0);
         emp.display();
     }
 }
+
