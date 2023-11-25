bee4b10a8e0607cce14d755edd6b37cc5ec5b8b3
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-25 11:52:02 +0400

    unifying the solution :+1:

--- labExamQ1.java
+++ labExamQ1.java
@@ -1,14 +1,11 @@
-// Q1: Write a Java program that contains a class named Circle with an instance variable: radius. The class should have the following methods: 1. assignRadius(): This method assigns a value to the radius of the circle using parameter passed from main method. 2. area(): This method calculates
-// and returns the area of the circle(Use the formula 3.14 * radius * radius). 3. circumference(): This method calculates and returns the circumference of the circle( Use the formula 2 * 3.14 *radius). Additionally, create another class named CircleOperations that contains the main method. Within the main method: Create an object of the Circle class. Use the object to call the
-// assignRadius method and assign a sample value to the radius. Calculate and display the area of the circle using the area method. Calculate and display the circumference of the circle using the
-// circumference method
+// Q1: Write a Java program that contains a class named Circle with an instance variable: radius. The class should have the following methods: 1. assignRadius(): This method assigns a value to the radius of the circle using parameter passed from main method. 2. area(): This method calculates and returns the area of the circle(Use the formula 3.14 * radius * radius). 3. circumference(): This method calculates and returns the circumference of the circle( Use the formula 2 * 3.14 *radius). Additionally, create another class named CircleOperations that contains the main method. Within the main method: Create an object of the Circle class. Use the object to call the
+// assignRadius method and assign a sample value to the radius. Calculate and display the area of the circle using the area method. Calculate and display the circumference of the circle using the circumference method
 
 class Circle {
     double radius;
 
-    double assignRadius(double r) {
-        radius = r;
-        return radius;
+    void assignRadius(double radius) {
+        this.radius = radius;
     }
 
     double area() {
@@ -22,10 +19,10 @@ class Circle {
 
 class CircleOperations {
     public static void main(String[] args) {
-        Circle ci = new Circle();
-
-        ci.assignRadius(33.5);
-        System.out.println("The area of the circle is " + ci.area());
-        System.out.println("The circumference of the circle is " + ci.circumference());
+        Circle c = new Circle();
+        c.assignRadius(5.0);
+        System.out.println("Area: " + c.area());
+        System.out.println("Circumference: " + c.circumference());
     }
 }
+
