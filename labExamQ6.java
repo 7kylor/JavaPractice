108b7f2dbed7e5dce2cc0cefb4687c8505118a57
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-25 12:18:23 +0400

    good to go

--- labExamQ6.java
+++ labExamQ6.java
@@ -1,14 +1,4 @@
-// Q6: Define a class called Product with three data members: productId, name,
-// and price.
-// Appropriate data types are to be chosen. The class should also have the
-// necessary set and get
-// member functions to set the values for the data members and to return the
-// values of the data
-// members respectively. Define another class that hosts the main method. Inside
-// the main
-// method: Create two instances/objects of the Product class. Call the set and
-// get methods using
-// the instance/objects created to display their functionality.
+// Q6: Define a class called Product with three data members: productId, name, and price. Appropriate data types are to be chosen. The class should also have the necessary set and get member functions to set the values for the data members and to return the values of the data members respectively. Define another class that hosts the main method. Inside the main method: Create two instances/objects of the Product class. Call the set and get methods using the instance/objects created to display their functionality.
 
 class Product {
     int productId;
--- labExamQ8.java
+++ labExamQ8.java
@@ -1,7 +1,6 @@
 // 8. Write the code for each class and include the necessary methods as described below:
 // Animal class: Method: void eat() - prints "Animal is eating." ,Method: void sleep() - prints
-// "Animal is sleeping." Mammal class (inherits from Animal): Method: void giveBirth() - prints "Mammal is giving birth."
-// Bird class (inherits from Animal): Method: void fly() - prints "Bird is flying."
+// "Animal is sleeping." Mammal class (inherits from Animal): Method: void giveBirth() - prints "Mammal is giving birth." Bird class (inherits from Animal): Method: void fly() - prints "Bird is flying."
 // Reptile class (inherits from Animal): Method: void crawl() - prints "Reptile is crawling."
 // Dog class (inherits from Mammal): Method: void bark() - prints "Dog is barking."
 // Eagle class (inherits from Bird): Method: void hunt() - prints "Eagle is hunting."
