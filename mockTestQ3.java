f7b5c2bff23ff68e16b37d796ecab1b0c2b7ad79
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-26 13:51:49 +0400

    Create mockTestQ3.java

---
+++ mockTestQ3.java
@@ -0,0 +1,47 @@
+// Q3 Given an interface called Tax with a data member taxRate that has a value
+// of 0.05 and member functions/methods calcTax() to calculate the income tax,
+// calcNet() to calculate the net salary and display() to display the gross
+// salary, the income tax and the net salary.
+// Note: Tax Amount is calculated as salary * taxrate.Net salary is calculated
+// as gross salary-Tax Amount.
+
+// >> note this question is smiler to labExamQ17.java //
+
+interface Tax {
+    double taxRate = 0.05;
+
+    double calcTax(double salary);
+
+    double calcNet(double salary);
+
+    void display(double salary);
+}
+
+class Salary implements Tax {
+    double grossSalary;
+
+    Salary(double grossSalary) {
+        this.grossSalary = grossSalary;
+    }
+
+    public double calcTax(double salary) {
+        return salary * taxRate;
+    }
+
+    public double calcNet(double salary) {
+        return salary - calcTax(salary);
+    }
+
+    public void display(double salary) {
+        System.out.println("Gross Salary: " + salary);
+        System.out.println("Tax: " + calcTax(salary));
+        System.out.println("Net Salary: " + calcNet(salary));
+    }
+}
+
+class TaxMain {
+    public static void main(String[] args) {
+        Salary s = new Salary(100000.0);
+        s.display(s.grossSalary);
+    }
+}
