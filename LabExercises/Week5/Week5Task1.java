1104e2d4b34e9659ce94fe091daf103a1f1122ad
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-27 10:34:36 +0400

    Update Week5Task1.java

---
+++ LabExercises/Week5/Week5Task1.java
@@ -0,0 +1,21 @@
+// Write a Java program to implement the concept of polymorphism using method
+// overloading in
+// class called Box. The class should have 3 instance variables called length
+// and breadth and
+// height. Also three setValues() methods to set initial values to the
+// variables. The first set method
+// has no parameters and should initialize the instance variables with some
+// values. The second
+// set method should have three parameters to initialize the variables using the
+// values passed
+// from main method. The third method should have one parameter which should be
+// used to set
+// the values for all the three class variables. The class should also have a
+// method called as
+// display() which displays the length, breadth and height of the box and also a
+// message whether
+// it is a normal box or a cube. Create another class that hosts the main
+// method. Create an object
+// of the Box class which invokes the all the three setValues() methods. Call
+// the display() method
+// also.
