86079fc3f3bb0df92b36886d23c3e4341094fdaf
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-27 07:56:00 +0400

    fixes

---
+++ practiceSlide16Week5.java
@@ -0,0 +1,15 @@
+// Create a class called "Employee" with attributes name, employee ID, and
+// salary.
+// Implement a parameterized constructor and non- parameterized constructor to
+// initialize
+// these attributes.
+// In a separate class, create an instance of the EmpDis class, display the
+// information of
+// employees by calling the overloaded constructor methods.
+
+class Employee {
+    String name;
+    int employeeID;
+    double salary;
+
+}
