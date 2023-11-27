fe92e82eaf1e72dacca53fe30f41d56b50e8077a
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-27 07:57:47 +0400

    Update practiceSlide16Week5.java

--- practiceSlide16Week5.java
+++ practiceSlide16Week5.java
@@ -9,7 +9,33 @@
 
 class Employee {
     String name;
-    int employeeID;
+    int employeeID; // i'd use long or string but whatever
     double salary;
 
+    Employee(String name, int employeeID, double salary) {
+        this.name = name;
+        this.employeeID = employeeID;
+        this.salary = salary;
+    }
+
+    Employee() {
+        this.name = "Taher";
+        this.employeeID = 10;
+        this.salary = 0.0;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Employee ID: " + employeeID);
+        System.out.println("Salary: " + salary);
+    }
+}
+
+class EmployeeMain {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Taher", 10, 0.0);
+        Employee e2 = new Employee();
+        e1.displayInfo();
+        e2.displayInfo();
+    }
 }
