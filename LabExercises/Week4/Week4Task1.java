e5ecf3654b10a16f3b50b9135915ce6fe524c762
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-27 09:11:12 +0400

    Week 4

---
+++ LabExercises/Week4/Week4Task1.java
@@ -0,0 +1,54 @@
+// Create a class called Box having variables length , L, breadth,B and
+// height,H. Also the following
+// methods i. Method giveValues() to assign values to the variables with the
+// values passed as
+// parameter from the main method ii. Method calcArea() to calculate the area of
+// the box and display all the area. The
+// formula to calculate the area of the box is 2(LB + LH + BH) iii. Method
+// calcVolume() to calculate the volume of the box and display all the
+// volume. The formula to calculate the volume of the box is L* B* H Create
+// another class that hosts the main method. And create 3 objects of the Box
+// class. Call all
+// the methods to check their functionality
+
+class Box {
+    double length;
+    double breadth;
+    double height;
+
+    void giveValues(double length, double breadth, double height) {
+        this.length = length;
+        this.breadth = breadth;
+        this.height = height;
+    }
+
+    void calcArea() {
+        double area = 2 * (length * breadth + length * height + breadth * height);
+        System.out.println("Area: " + area);
+    }
+
+    void calcVolume() {
+        double volume = length * breadth * height;
+        System.out.println("Volume: " + volume);
+    }
+}
+
+class BoxMain {
+    public static void main(String[] args) {
+        Box box1 = new Box();
+        Box box2 = new Box();
+        Box box3 = new Box();
+
+        box1.giveValues(1, 2, 3);
+        box2.giveValues(4, 5, 6);
+        box3.giveValues(7, 8, 9);
+
+        box1.calcArea();
+        box2.calcArea();
+        box3.calcArea();
+
+        box1.calcVolume();
+        box2.calcVolume();
+        box3.calcVolume();
+    }
+}
