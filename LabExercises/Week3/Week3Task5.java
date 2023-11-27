5f406c92ea8dde98e857d5644d1ab94a99d65100
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-27 08:59:47 +0400

    Update Week3Task5.java

--- LabExercises/Week3/Week3Task5.java
+++ LabExercises/Week3/Week3Task5.java
@@ -9,3 +9,59 @@
 // class. Call the set and get methods using the instance/object created to
 // display their
 // functionality.
+
+class Animal {
+    String name;
+    int age;
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+
+    }
+}
+
+class Dog extends Animal {
+    String breed;
+    String sound;
+
+    void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    void setSound(String sound) {
+        this.sound = sound;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+}
+
+class AnimalMain {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+
+        dog.setName("Dog");
+        dog.setAge(5);
+        dog.setBreed("Labrador");
+        dog.setSound("Woof");
+
+        System.out.println("Dog: " + dog.getName() + " " + dog.getAge() + " " + dog.getBreed() + " " + dog.getSound());
+    }
+}
