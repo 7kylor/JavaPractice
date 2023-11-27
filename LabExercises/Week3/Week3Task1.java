270311384bcde819649904381cb604e79f0e6e63
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-27 08:36:42 +0400

    Update Week3Task1.java

--- LabExercises/Week3/Week3Task1.java
+++ LabExercises/Week3/Week3Task1.java
@@ -11,3 +11,55 @@
 // method. Inside the main method: Create an instance/object of the BankAccount
 // class. Call all
 // the methods using the instance/object created to display their functionality.
+
+class BankAccount {
+
+    double balance;
+    int accountNumber;
+
+    BankAccount(double balance, int accountNumber) {
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+    }
+
+    void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    double getBalance() {
+        return balance;
+    }
+
+    int getAccountNumber() {
+        return accountNumber;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    void display() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+    }
+
+}
+
+class BankAccountOperations {
+    public static void main(String[] args) {
+        BankAccount b = new BankAccount(1000.0, 1245489);
+        b.display();
+        b.deposit(100.0);
+        b.display();
+        b.withdraw(50.0);
+        b.display();
+    }
+}
