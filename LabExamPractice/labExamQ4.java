8c5b9dd79b25661e42fda2e67cae3c9b672235c5
Taher Al Kiyumi <20871728+7kylor@users.noreply.github.com>
2023-11-28 12:08:12 +0400

    Update labExamQ4.java

--- LabExamPractice/labExamQ4.java
+++ LabExamPractice/labExamQ4.java
@@ -1,47 +1,47 @@
 // Q4:  Define a class called BankAccount with two data members: balance and Account Number .This class should also have the following. a) constructor to initialize the variables with values passed from main method using parameters. b) set()and get() methods for setting value and returning value for the class variables. c) method deposit() to add an amount to the balance. d) a method withdraw() to deduct an amount from the balance. Define another class that hosts the main method. Inside the main method: Create an instance/object of the BankAccount class. Call all the methods using the instance/object created to display their functionality.
 
 class BankAccount {
-    double balance;
-    String accountNumber;
+    private double balance;
+    private int accountNumber;
 
-    BankAccount(double balance, String accountNumber) {
+    public BankAccount(double balance, int accountNumber) {
         this.balance = balance;
         this.accountNumber = accountNumber;
     }
 
-    void setBalance(double balance) {
+    public void setBalance(double balance) {
         this.balance = balance;
     }
 
-    void setAccountNumber(String accountNumber) {
+    public void setAccountNumber(int accountNumber) {
         this.accountNumber = accountNumber;
     }
 
-    double getBalance() {
+    public double getBalance() {
         return balance;
     }
 
-    String getAccountNumber() {
+    public int getAccountNumber() {
         return accountNumber;
     }
 
-    void deposit(double amount) {
+    public void deposit(double amount) {
         balance += amount;
     }
 
-    void withdraw(double amount) {
+    public void withdraw(double amount) {
         balance -= amount;
     }
 }
 
-class BankAccountMain {
+public class labExamQ4 {
     public static void main(String[] args) {
-        BankAccount b = new BankAccount(1000.0, "1234567890");
-        System.out.println("Balance: " + b.getBalance());
-        System.out.println("Account Number: " + b.getAccountNumber());
-        b.deposit(100.0);
-        System.out.println("Balance: " + b.getBalance());
-        b.withdraw(50.0);
-        System.out.println("Balance: " + b.getBalance());
+        BankAccount account = new BankAccount(1000, 123456789);
+        System.out.println("Account Number: " + account.getAccountNumber());
+        System.out.println("Balance: " + account.getBalance());
+        account.deposit(100);
+        System.out.println("Balance after deposit: " + account.getBalance());
+        account.withdraw(50);
+        System.out.println("Balance after withdraw: " + account.getBalance());
     }
 }
